febd69974f666b4486c977f3189c452325ffa7f1
lefessan <fabrice@lefessant.net>
2021-09-08 10:03:57 +0000

    Deploying to gh-pages from  @ fc5dc6620dd94dd635f6f3ded2e79c8202468261 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (95f1472) (2021/05/21 10:09)',
+    VERSION: 'master (fc5dc66) (2021/09/08 10:03)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
